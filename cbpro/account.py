398983977ac588021876216e15a1895fc238f595
John Rangel <rangel.john.j@gmail.com>
2021-11-22 10:52:01 -0600

    Major code rewite to make 'account' objects the major data object, not 'portfolio' objects.

---
+++ cbpro/account.py
@@ -0,0 +1,161 @@
+"""Individual Coinbase Pro trading account."""
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+# cbpro toolset:
+from cbpro.markets import price_history
+from cbpro._api import apiwrapper
+import cbpro._utilities as utils
+
+# Pandas Index Slices:
+idx = pd.IndexSlice
+
+# accounts class:
+class account(
+    apiwrapper,
+    ):
+    def __init__(
+        self,
+        name,
+        account_id,
+        api_key_file=None,
+        ):
+        apiwrapper.__init__(self)
+        self.read_keyfile(api_key_file)
+        self.name = name
+        self.account_id = account_id
+        self.ledger=None
+        self.usd_fills=None
+        self.deposits=None
+        self.performance_data=None
+        self._url_setup()
+        
+    def get_ledger(self):
+        try:
+            query_output = self.query(self.LEDGER_URL)
+            self._setup_ledger(query_output)
+            print("%s account loaded successfully..."%self.name)
+        except:
+            self.ledger = None
+            print("%s account empty..."%self.name)
+
+    def return_ledger(self):
+        if type(self.ledger) is pd.DataFrame:
+            return self.ledger.copy()
+        else:
+            return None
+
+    def get_usd_fills(self):
+        query_output = self.query(self.USD_FILLS_URL)
+        self._setup_fills(query_output)
+    
+    def return_usd_fills(self):
+        if type(self.usd_fills) is pd.DataFrame:
+            return self.usd_fills.copy()
+        else:
+            return None
+
+    def extract_deposits(self):
+        usd_fills = self.return_usd_fills()
+        usd_fills = usd_fills[
+            (usd_fills.side=="buy")
+            ].resample("D"
+            ).sum(
+            )
+        cols = [
+            "usd",
+            "coin",
+            ]
+        deposits = utils.new_history_df(
+            cols,
+            start=self.start_date,
+            end=self.end_date,
+            )
+        tdates = usd_fills.index
+        deposits.loc[tdates,"usd"] = usd_fills.usd_volume
+        self.deposits = deposits
+
+    def return_deposits(self):
+        if type(self.deposits) is pd.DataFrame:
+            return self.deposits.copy()
+        else:
+            return None
+
+    def extract_performance(
+        self,
+        granularity=86400, #daily
+        ):
+        ph = price_history(
+            pair="%s-USD"%self.name,
+            start=self.start_date,
+            end=self.end_date,
+            granularity=granularity,
+            )
+        cols = [
+            "usd_deposits",
+            "number_of_coins",
+            ]
+        df = utils.new_history_df(
+            cols,
+            start=self.start_date,
+            end=self.end_date,
+            )
+        deposits = self.return_deposits()
+        ledger = self.return_ledger().resample("D").last()
+        df.loc[deposits.index,"usd_deposits"] = deposits.usd.cumsum()
+        df.loc[ledger.index,"number_of_coins"] = ledger.balance.copy()
+        df["coin_price"] = ph.open.copy()
+        df = df.ffill()
+        df["coin_usd_value"] = df.coin_price*df.number_of_coins
+        df["performance"] = df.coin_usd_value/df.usd_deposits
+        self.performance_data = df
+    
+    def return_performance_data(self):
+        if type(self.performance_data) is pd.DataFrame:
+            return self.performance_data.copy()
+        else:
+            return None
+
+    def _setup_ledger(self,query_output):
+        df = pd.DataFrame(query_output)
+        df.loc[:,"id"] = df.id.apply(int)
+        self._unnest_dict(df)
+        self._correct_datetime(df)
+        for col in ["amount","balance"]:
+            df.loc[:,col] = df[col].apply(float)
+        self.ledger=df.set_index("created_at")
+        self.start_date = df.created_at.iloc[-1].date()
+        self.end_date = datetime.now().date()
+    
+    def _setup_fills(self,query_output):
+        df = pd.DataFrame(query_output)
+        self._correct_datetime(df)
+        for col in ["price","size","fee","usd_volume"]:
+            df.loc[:,col] = df[col].apply(float)
+        self.usd_fills = df.set_index("created_at")
+
+    def _unnest_dict(self,df):
+        for col in ["order_id","product_id","trade_id"]:
+            try:
+                data = df.details.apply(lambda x: x[col])
+                df[col] = data
+            except:
+                pass
+
+    def _correct_datetime(self,df):
+        df.loc[:,"created_at"] = pd.to_datetime(df.created_at)
+        df.loc[:,"created_at"] = df.created_at.apply(
+            lambda x: x.replace(tzinfo=None),
+            )
+
+    def _url_setup(self,fills_number=100):
+        self.LEDGER_URL = "/accounts/%s/ledger"%(self.account_id)
+        self.USD_FILLS_URL = "/fills?product_id=%s-USD"%(self.name)
+
+
+
+
+
+
+
--- cbpro/markets.py
+++ cbpro/markets.py
@@ -3,119 +3,98 @@ import pandas as pd
 import datetime
 
 # internal functions:
-import cbpro._utilities as utils
 from cbpro._api import apiwrapper
+import cbpro._utilities as utils
 
 # Pandas index slice:
 idx = pd.IndexSlice
 
-class markets(apiwrapper):
-    def __init__(
-        self,
-        name,
-        ):
-        apiwrapper.__init__(self)
-        self.name=name
-
-    def available_products(self):
-        api_output = self.query("/products")
-        return pd.DataFrame(api_output)
+def price_history(
+    pair,
+    start,
+    end,
+    granularity,
+    debug=False,
+    ):
+    # The max number of data per request is 300 candles. 
+    # If the request is larger than 300 candles, the API 
+    # will reject it. 
+    # 
+    # granularity must be one of these values:
+    # 60         (one minute)
+    # 300         (five minutes)
+    # 900         (fifteen minutes)
+    # 3600         (one hour)
+    # 21600     (six hours)
+    # 86400     (one day)
+    # 
+    # create empty dataframe for the prices:
+    mdf_index = pd.date_range(
+        start=start,
+        end=end,
+        freq="%dS"%granularity,
+        )
     
-    def usd_products(self):
-        df = self.available_products()
-        return df[df.quote_currency=="USD"].reset_index(drop=True)
-        
-    def stablecoin_products(self):
-        df = self.available_products()
-        return df[df.fx_stablecoin==True].reset_index(drop=True)
-
-    def price_history(
-        self,
-        pair,
-        start,
-        end,
-        granularity, 
-        debug=False,
-        ):
-        # The max number of data per request is 300 candles. 
-        # If the request is larger than 300 candles, the API 
-        # will reject it. 
-        # 
-        # granularity must be one of these values:
-        # 60         (one minute)
-        # 300         (five minutes)
-        # 900         (fifteen minutes)
-        # 3600         (one hour)
-        # 21600     (six hours)
-        # 86400     (one day)
-        # 
-        # create empty dataframe for the prices:
-        mdf_index = pd.date_range(
-            start=start,
-            end=end,
-            freq="%dS"%granularity,
-            )
-        mdf = pd.DataFrame(
-            np.nan,
-            index=mdf_index,
-            columns=[
-                "low",
-                "high",
-                "open",
-                "close",
-                "volume",
-                ],
-            )
-        #mdf = mdf.set_index("time")
-        
-        # discretize the datetime index into groups of 300
-        # if necessary:
-        index_slices = []
-        if len(mdf_index) >= 300:
-            start = 0
-            end = 299
-            num_slices,remainder = divmod(len(mdf_index),300)
-            for ii in range(num_slices):
-                index_slices.append([
-                    mdf_index[start],
-                    mdf_index[end],
-                    ])
-                start += 300
-                end += 300
+    # discretize the datetime index into groups of 300
+    # if necessary:
+    index_slices = []
+    if len(mdf_index) >= 300:
+        start = 0
+        end = 299
+        num_slices,remainder = divmod(len(mdf_index),300)
+        for ii in range(num_slices):
             index_slices.append([
-                mdf_index[-remainder],
-                mdf_index[-1]
+                mdf_index[start],
+                mdf_index[end],
                 ])
-        else:
-            index_slices.append([
-                mdf_index[0],
-                mdf_index[-1],
-                ])
+            start += 300
+            end += 300
+        index_slices.append([
+            mdf_index[-remainder],
+            mdf_index[-1]
+            ])
+    else:
+        index_slices.append([
+            mdf_index[0],
+            mdf_index[-1],
+            ])
+    
+    # iterate over each (start, end) pair:
+    cbapi = apiwrapper()
+    endpoint_template = "/products/{product_id}/candles?{options}"
+    frames = []
+    for start, end in index_slices:
+        iso_start = start.strftime("%Y-%m-%dT%H:%M:%SZ")
+        iso_end = end.strftime("%Y-%m-%dT%H:%M:%SZ")
+        query_options = "start=%s&end=%s&granularity=%d"%(
+            iso_start,
+            iso_end,
+            granularity,    
+            )
+        endpoint = endpoint_template.format(
+            product_id=pair,
+            options=query_options,
+            )
         
-        # iterate over each (start, end) pair:
-        endpoint_template = "/products/{product_id}/candles?{options}"
-        for start, end in index_slices:
-            iso_start = start.strftime("%Y-%m-%dT%H:%M:%SZ")
-            iso_end = end.strftime("%Y-%m-%dT%H:%M:%SZ")
-            query_options = "start=%s&end=%s&granularity=%d"%(
-                iso_start,
-                iso_end,
-                granularity,    
-                )
-            endpoint = endpoint_template.format(
-                product_id=pair,
-                options=query_options,
-                )
-            
-            # query API:
-            api_output = self.query(endpoint,debug=debug)
-            
-            # store in multiindex dataframe:
-            df = utils.format_price_history(api_output)
-            mdf.loc[start:end] = df
+        # query API:
+        api_output = cbapi.query(endpoint,debug=debug)
         
-        # return multiindex dataframe:
-        return mdf
+        # store in multiindex dataframe:
+        df = pd.DataFrame(api_output)
+        frames.append(df)
+    
+    # create and return multiindex dataframe:
+    results = pd.concat(frames)
+    results.loc[:,0] = pd.to_datetime(results[0], unit="s")
+    results.columns = [
+        "date",
+        "low",
+        "high",
+        "open",
+        "close",
+        "volume",
+        ]
+    return results.set_index("date")
 
 
 
