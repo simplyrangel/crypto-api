10753016a48f3ca013b044a915d210ba814049e7
John Rangel <rangel.john.j@gmail.com>
2021-11-28 10:21:14 -0600

    new_history_df() added.

--- kucoin/_utilities.py
+++ kucoin/_utilities.py
@@ -1,4 +1,6 @@
 """Utilities to support KuCoin account class."""
+import numpy as np
+import pandas as pd
 from datetime import datetime
 
 def update_createdAt(df):
@@ -37,3 +39,22 @@ def fill_request_url(
         ]
     options = "&".join(options)
     return "%s?%s"%(endpoint,options)
+
+def new_history_df(
+    columns,
+    start,
+    end,
+    frequency="D",
+    ):
+    df_index = pd.date_range(
+        start,
+        end,
+        freq=frequency,
+        tz=None,
+        )
+    return pd.DataFrame(
+        np.nan,
+        index=df_index,
+        columns=columns,
+        )
+    
